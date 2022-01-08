725f93097c742384b41e07a745d8924b4b29da86
houcem khlifi <khlifihoucem@zaghouan.r-iset.tn>
2022-01-08 08:46:39 +0000

    Add new file

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script has been tested on Ubuntu 20.04
+# For other versions of Ubuntu, you might need some tweaking
+
+echo "[TASK 1] Install containerd runtime"
+apt update -qq >/dev/null 2>&1
+apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+mkdir /etc/containerd
+containerd config default > /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null 2>&1
+
+echo "[TASK 2] Add apt repo for kubernetes"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+
+echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
+systemctl restart kubelet
+
+echo "[TASK 4] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 5] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 6] Install additional packages"
+apt install -qq -y net-tools >/dev/null 2>&1
